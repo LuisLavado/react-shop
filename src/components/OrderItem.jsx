af0c96b15cc383a4d6030907b136b0e4bad27091
Luis GitHub <luisito.ingenermax@gmail.com>
2023-03-26 03:17:55 -0500

    remoteItem Cart

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '@styles/OrderItem.scss';
 import iconClose from "@icons/icon_close.png";
+import AppContext from '../context/AppContext';
 
 const OrderItem = ({ product }) => {
+	const { removeFromCart } = useContext(AppContext);
+	const handleRemove = item => {
+		removeFromCart(item)
+	}
+
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -10,7 +16,7 @@ const OrderItem = ({ product }) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={iconClose} alt="close" />
+			<img src={iconClose} alt="close" onClick={() => handleRemove(product)} />
 		</div>
 	);
 }
